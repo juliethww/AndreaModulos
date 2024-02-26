ec80d68df196c9f1c6a8cf273a703bde9355231b
Andrea Silva <andreasilva23433@gmail.com>
2024-02-26 13:55:29 -0500

    realice cambios en el titulo

--- andrea/usuario.php
+++ andrea/usuario.php
@@ -45,7 +45,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Usuarios</title>
+	<title>Usuarios ANDREA</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
